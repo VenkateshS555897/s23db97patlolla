a59a85fd86f01dd98362db9650c621997f4e886e
VenkateshS555897 <venkateshreddy0918@gmail.com>
2023-04-14 14:14:09 -0500

    cm

--- controllers/grass.js
+++ controllers/grass.js
@@ -11,7 +11,7 @@ exports.grass_list = async function(_req, res) {
         }
 };
 
-// Handle Costume create on POST.
+// Handle grass create on POST.
 exports.grass_create_post = async function(req, res) {
     console.log(req.body)
     let document = new grass();
@@ -19,9 +19,9 @@ exports.grass_create_post = async function(req, res) {
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
     // {"grass_name":"image1", "size":"large", "cost":1000}
-    document.grass_name = req.body.grass_name;
-    document.size = req.body.size;
-    document.cost = req.body.cost;
+    document.Grass_Name = req.body.Grass_Name;
+    document.Grass_color = req.body.Grass_color;
+    document.Height = req.body.Height;
     // console.log(document);
     try{
     let result = await document.save();
@@ -67,7 +67,7 @@ exports.grass_update_put = async function(_req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
         }
-// res.send('NOT IMPLEMENTED: Gift update PUT' + req.params.id);
+ res.send('NOT IMPLEMENTED: Gift update PUT' + req.params.id);
 };
 
 
@@ -82,4 +82,37 @@ catch(err){
 res.status(500);
 res.send(`{"error": ${err}}`);
 }
+};
+
+// for a specific grass.
+exports.grass_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await grass.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+   // Handle grass update form on PUT.
+exports.grass_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await grass.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.Grass_Name)
+ toUpdate.Grass_Name = req.body.Grass_Name;
+ if(req.body.Grass_color) toUpdate.Grass_color = req.body.Grass_color;
+ if(req.body.Height) toUpdate.Height = req.body.Height;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
